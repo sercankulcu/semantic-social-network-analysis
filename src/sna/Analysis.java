185be651b1d27252b17659408847a8ec5d3d489d
sercan <sercankulcu@gmail.com>
2015-12-27 15:44:46 +0200

    removed hard coded parameters

--- src/sna/Analysis.java
+++ src/sna/Analysis.java
@@ -5,16 +5,23 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.webapp.WebAppContext;
 
+/*
+ * Main driver class
+ * crated web server instance and sets paths.
+ * runs server instance
+ * 
+ * */
 public class Analysis {
 
 	public static void main(String[] args) throws Exception
 	{
-		System.out.println("started");
+		
+		System.out.println("SSNA platform started..");
 		Server server = new Server(8080);
 
-		String warUrlString = "C:\\Users\\sercan\\workspace\\sna\\webroot";//warUrl.toExternalForm();
+		String warUrlString = "webroot";//warUrl.toExternalForm();
 	    
-	    System.out.println(warUrlString);
+	    System.out.println("Webroot path is " + warUrlString);
 
 	    // Create a webAppContext, pointing the "webroot" folder to the "/files" url. 
 	    // The files will be available at <server>:<port>/files
