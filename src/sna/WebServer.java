9a0f4a687ee0f0801a11cba15cca69614df602b4
sercan <sercankulcu@gmail.com>
2015-12-27 16:55:24 +0200

    comment lines added

--- src/sna/WebServer.java
+++ src/sna/WebServer.java
@@ -9,6 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
+/*
+ * User Interaction class
+ * prepares web page, takes user id input and loads generated .js files
+ * 
+ * */
+
 public class WebServer extends AbstractHandler {
 
 	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) 
@@ -17,13 +23,10 @@ public class WebServer extends AbstractHandler {
 		
 		response.setContentType("text/html;charset=utf-8");
 		response.setStatus(HttpServletResponse.SC_OK);
+		
 		baseRequest.setHandled(true);
-		response.getWriter().println("<table>");
-		response.getWriter().println("<tr><td><img src=\"files/etu.jpg\" alt=\"TOBB ETU\" style=\"width:80px;height:64px;\"></td>");
-		response.getWriter().println("<td><h1>Social Network Analysis</h1></td></tr></table>");
-		response.getWriter().println("<form method=\"post\" action=\"analysis\"><br></br>");
-		response.getWriter().println("<input type=\"text\" name=\"text\" value=\"Enter Twitter ID...\" size=\"40\" maxlength=\"40\" onfocus=\"this.value=''\">");
-		response.getWriter().println("<input type=\"submit\" value=\"Show Result!\" align=\"center\"></form>User ID: ");
+		
+		preparePage(response);
 
 		// after clicked submit button
 		if(baseRequest.toString().contains("analysis")) {
@@ -31,24 +34,39 @@ public class WebServer extends AbstractHandler {
 			String userid = request.getParameter("text");
 			response.getWriter().println(userid + "");
 			
-/*			TwitterInterface twitterInt = new TwitterInterface(userid);
+			TwitterInterface twitterInt = new TwitterInterface(userid);
 			twitterInt.getFollowers();
-	*/	
-			
+	
+			/*
 			TwitterInterface twitterInt = new TwitterInterface(userid);
 			twitterInt.test();
+			*/
 			
-			
-			response.getWriter().println("<script type=\"text/javascript\" src=\"files/dist/vis.js\"></script>");		//algorithm
-			response.getWriter().println("<script type=\"text/javascript\" src=\"files/dist/network2.js\"></script>");	//data
-			response.getWriter().println("<link href=\"files/dist/vis.css\" rel=\"stylesheet\" type=\"text/css\" />");
-			response.getWriter().println("<style type=\"text/css\"> #mynetwork { width: 1200px;  height: 700px;  border: 1px solid lightgray;} </style>");
-			response.getWriter().println("<div id=\"mynetwork\"></div>");
-			response.getWriter().println("<script type=\"text/javascript\">");
-			response.getWriter().println("var container = document.getElementById('mynetwork');");
-			response.getWriter().println("var data = { nodes: nodes,   edges: edges  };");
-			response.getWriter().println("var options = {};");
-			response.getWriter().println("var network = new vis.Network(container, data, options);</script>");
+			loadJavascript(response);
 		}
+	}
+	
+	private void preparePage(HttpServletResponse response) throws IOException {
+		
+		response.getWriter().println("<table>");
+		response.getWriter().println("<tr><td><img src=\"files/etu.jpg\" alt=\"TOBB ETU\" style=\"width:80px;height:64px;\"></td>");
+		response.getWriter().println("<td><h1>Social Network Analysis</h1></td></tr></table>");
+		response.getWriter().println("<form method=\"post\" action=\"analysis\"><br></br>");
+		response.getWriter().println("<input type=\"text\" name=\"text\" value=\"Enter Twitter ID...\" size=\"40\" maxlength=\"40\" onfocus=\"this.value=''\">");
+		response.getWriter().println("<input type=\"submit\" value=\"Show Result!\" align=\"center\"></form>User ID: ");
+	}
+	
+	private void loadJavascript(HttpServletResponse response) throws IOException {
+		
+		response.getWriter().println("<script type=\"text/javascript\" src=\"files/dist/vis.js\"></script>");		//algorithm
+		response.getWriter().println("<script type=\"text/javascript\" src=\"files/dist/network2.js\"></script>");	//data
+		response.getWriter().println("<link href=\"files/dist/vis.css\" rel=\"stylesheet\" type=\"text/css\" />");
+		response.getWriter().println("<style type=\"text/css\"> #mynetwork { width: 1200px;  height: 700px;  border: 1px solid lightgray;} </style>");
+		response.getWriter().println("<div id=\"mynetwork\"></div>");
+		response.getWriter().println("<script type=\"text/javascript\">");
+		response.getWriter().println("var container = document.getElementById('mynetwork');");
+		response.getWriter().println("var data = { nodes: nodes,   edges: edges  };");
+		response.getWriter().println("var options = {};");
+		response.getWriter().println("var network = new vis.Network(container, data, options);</script>");
 	}
 }
